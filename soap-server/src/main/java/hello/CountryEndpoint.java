219167a77ed61bb091292ffd004f2383a01a192c
amandzhi <amandzhi@gmail.com>
2019-12-16 22:54:12 +0300

    soap_spring

---
+++ soap-server/src/main/java/hello/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package hello;
+
+
+import amandzhi.GetCountryRequest;
+import amandzhi.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+//http://spring.io/guides/gs-producing-web-service
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "amandzhi";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
